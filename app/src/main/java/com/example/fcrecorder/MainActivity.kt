3ae80d0989ff799bec9fdaff30aa990ff3a0b30a
David Yoon <qqpo12@naver.com>
2024-08-28 21:31:16 +0900

    First Commit
    * Set default layout

---
+++ app/src/main/java/com/example/fcrecorder/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.fcrecorder
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fcrecorder.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        
+
+
+    }
+}
